781cc95c405642133115a04ee11df08687b47c54
Omar Feria <OFeria@SedgwickCMS.com>
2022-08-04 15:39:51 -0500

    Commit  --Adding Root Variable

--- AutomationLibrary/AutomationLibrary/ClsVariables.cs
+++ AutomationLibrary/AutomationLibrary/ClsVariables.cs
@@ -34,10 +34,12 @@ namespace AutomationLibrary
         /// Constant to define the Automation Setting driver
         /// </summary>
         public static string strGlobalConfigFile => blLocalExecution ? @"C:\AutomationProjects\AutomationSettings.xlsx" : @"H:\Any\4th_Automation\GlobalIntakeDriver\AutomationSettings.xlsx";
+        public static string strGlobalRootDirectory => blLocalExecution ? @"C:\AutomationProjects\" : @"H:\Any\4th_Automation\GlobalIntakeDriver\";
 
         /// <summary>
         /// Constant static for Edge Driver
         /// </summary>
         public static string strEdgeDriverPath = @"H:\Any\4th_Automation\GlobalIntakeDriver\ExternalLib\";
+
     }
 }
